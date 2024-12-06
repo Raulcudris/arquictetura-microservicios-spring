42ad44c76d3d2ef6a7ead3ebcbee72b7d503cd95
RaulCudris <raulcudriz@hotmail.com>
2024-12-06 12:11:01 -0500

    Actualizacion de microservicios

---
+++ screenshots/src/main/java/com/makiia/screenshots/ScreenshotsApplication.java
@@ -0,0 +1,13 @@
+package com.makiia.screenshots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenshotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenshotsApplication.class, args);
+	}
+
+}
---
+++ screenshots/src/test/java/com/makiia/screenshots/ScreenshotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makiia.screenshots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenshotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
